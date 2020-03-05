64973968203fc2163493e992499a96015edc9113
DeltaDizzy <EJBraswell@gmail.com>
2020-03-04 23:53:00 -0600

    make robot a property dump

--- FRSP-LINQ/Parsing/Robot.cs
+++ FRSP-LINQ/Parsing/Robot.cs
@@ -8,240 +8,33 @@ namespace FRSP_LINQ.Parsing
 {
     public class Robot
     {
-        //public static Robot self;
 
-        public Robot()
-        {
+        public int BallsAutoLower { get; set; }
 
-        }
+        public int BallsAutoOuter { get; set; }
 
-        // all game events and data points. this class is a liason between the UI itself and the VM
+        public int BallsAutoInner { get; set; }
 
-        #region Autonomous
-        private int ballsAutoLower = 0; // 2pts
-        private int ballsAutoOuter = 0; // 4pts
-        private int ballsAutoInner = 0; // 6pts
-        private bool crossedAutoLine = false; // true if the robot moves during auto
+        public bool CrossedAutoLine { get; set; }
 
-        //[Index(3)]
-        public int BallsAutoLower
-        {
-            get
-            {
-                return ballsAutoLower;
-            }
-            set
-            {
-                ballsAutoLower = value;
-            }
-        }
+        public int BallsTeleLower { get; set; }
 
-        //[Index(4)]
-        public int BallsAutoOuter
-        {
-            get
-            {
-                return ballsAutoOuter;
-            }
-            set
-            {
-                ballsAutoOuter = value;
-            }
-        }
+        public int BallsTeleOuter { get; set; }
 
-        //[Index(5)]
-        public int BallsAutoInner
-        {
-            get
-            {
-                return ballsAutoInner;
-            }
-            set
-            {
-                ballsAutoInner = value;
-            }
-        }
+        public int BallsTeleInner { get; set; }
 
-        //[Index(6)]
-        public bool CrossedAutoLine
-        {
-            get
-            {
-                return crossedAutoLine;
-            }
-            set
-            {
-                crossedAutoLine = value;
-            }
-        }
-        #endregion
+        public bool WheelRotation { get; set; }
+        
+        public bool WheelPosition { get; set; }
 
-        #region Teleop
-        private int ballsTeleLower = 0; // 1pt
-        private int ballsTeleOuter = 0; // 2pts
-        private int ballsTeleInner = 0; // 3pts
-        private bool wheelRotationControl = false; // 10pts
-        private bool wheelPositionControl = false; // 20pts
+        public bool CanHang { get; set; }
 
-        //[Index(7)]
-        public int BallsTeleLower
-        {
-            get
-            {
-                return ballsTeleLower;
-            }
-            set
-            {
-                ballsTeleLower = value;
-            }
-        }
+        public bool IsLevel { get; set; }
 
-        //[Index(8)]
-        public int BallsTeleOuter
-        {
-            get
-            {
-                return ballsTeleOuter;
-            }
-            set
-            {
-                ballsTeleOuter = value;
-            }
-        }
+        public int TeamNumber { get; set; }
 
-        //[Index(9)]
-        public int BallsTeleInner
-        {
-            get
-            {
-                return ballsTeleInner;
-            }
-            set
-            {
-                ballsTeleInner = value;
-            }
-        }
+        public int MatchNumber { get; set; }
 
-        //[Index(10)]
-        public bool WheelRotation
-        {
-            get
-            {
-                return wheelRotationControl;
-            }
-            set
-            {
-                wheelRotationControl = value;
-            }
-        }
-
-        //[Index(10)]
-        public bool WheelPosition
-        {
-            get
-            {
-                return wheelPositionControl;
-            }
-            set
-            {
-                wheelPositionControl = value;
-            }
-        }
-        #endregion
-
-        #region Endgame
-        private bool hang = false; // 25pts
-        //private bool balance; // neat
-        private bool level = false; // 15pts
-
-        //[Index(11)]
-        public bool CanHang
-        {
-            get
-            {
-                return hang;
-            }
-            set
-            {
-                hang = value;
-            }
-        }
-
-        //[Index(13)]
-        public bool IsLevel
-        {
-            get
-            {
-                return level;
-            }
-            set
-            {
-                level = value;
-            }
-        }
-        #endregion
-
-        #region Misc
-
-        private int? _teamNum;
-        private int _matchNum = 1;
-        private String _watchPos = string.Empty;
-
-        //[Index(1)]
-        public int TeamNumber
-        {
-            get
-            {
-                return _teamNum ?? 0;
-            }
-            set
-            {
-                _teamNum = value;
-            }
-        }
-
-        //[Index(0)]
-        public int MatchNumber
-        {
-            get
-            {
-                return _matchNum;
-            }
-            set
-            {
-                _matchNum = value;
-            }
-        }
-
-
-        public String WatchPos
-        {
-            get
-            {
-                return _watchPos;
-            }
-            set
-            {
-                _watchPos = value;
-            }
-        }
-        #endregion
-
-        public void Reset()
-        {
-            CrossedAutoLine = false;
-            BallsAutoInner = 0;
-            BallsAutoLower = 0;
-            BallsAutoOuter = 0;
-            BallsTeleInner = 0;
-            BallsTeleOuter = 0;
-            BallsTeleLower = 0;
-            CanHang = false;
-            IsLevel = false;
-            WheelPosition = false;
-            WheelRotation = false;
-            MatchNumber += 1;
-            TeamNumber = 0000;
-        }
+        public String WatchPos { get; set; }
     }
 }
