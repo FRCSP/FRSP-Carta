44fe1bcfcab21be392585fefeef8c88c3d69f708
DeltaDizzy <ejbraswell@gmail.com>
2020-03-06 11:05:08 -0600

    wpf

---
+++ FRSP_Carta/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace FRSP_Carta.ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            MyModel = new PlotModel { Title = "Carta" };
+            MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public PlotModel MyModel
+        {
+            get;
+            private set;
+        }
+    }
+}
