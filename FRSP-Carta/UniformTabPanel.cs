d576b3d859da6c545d81e57b2e86fb8585262ee2
DeltaDizzy <ejbraswell@gmail.com>
2020-03-09 18:13:29 -0500

    restart w/ no pkg

---
+++ FRSP-Carta/UniformTabPanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace FRSP_Carta
+{
+    public class UniformTabPanel : UniformGrid
+    {
+        public UniformTabPanel()
+        {
+            this.IsItemsHost = true;
+            this.Rows = 1;
+
+            this.HorizontalAlignment = HorizontalAlignment.Stretch;
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            var totalMaxWidth = this.Children.OfType<TabItem>().Sum(tab => tab.MaxWidth);
+            if (!double.IsInfinity(totalMaxWidth))
+            {
+                this.HorizontalAlignment = (constraint.Width > totalMaxWidth)
+                    ? HorizontalAlignment.Left
+                    : HorizontalAlignment.Stretch;
+            }
+
+            return base.MeasureOverride(constraint);
+        }
+    }
+}
